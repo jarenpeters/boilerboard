08d8ed3e01bb8b2f79a89415026c1ea9e3350fb7
Jaren Peters <jarenpeters10@gmail.com>
2025-12-04 19:55:37 -0500

    Update About page content and styling

--- front-end/src/Pages/About.jsx
+++ front-end/src/Pages/About.jsx
@@ -3,8 +3,12 @@ import React from "react"
 export default function About() {
     return (
         <div className="text-white">
-            <div className="mx-30 my-35 text-4xl font-dmsans font-extrabold text-center">
-                <p>“67” is more than just a number—it’s a cultural moment born from the viral song Doot Doot (6 7) by Skrilla and fueled by TikTok edits, basketball highlights, and the chaotic energy of Gen Z and Gen Alpha humor. What started as a simple chant referencing LaMelo Ball’s height and a viral clip of a kid shouting “six-seven” has exploded into classrooms, social media feeds, and everyday slang. With no fixed meaning, “67” thrives on randomness, community, and the shared joy of something that makes no sense but somehow connects everyone who says it.</p>
+            <h1 className="text-center -pb-6 -mt-10 text-[100px] font-dmsans font-black"><em>made by jaren peters</em></h1>
+            <div className="flex grid grid-cols-2 my-20 -mt-2 text-2xl font-dmsans font-extrabold text-center">
+                <img src="https://jarenpeters.com/jarn1.ico" className="ml-20 w-170 h-120"/>
+                <div>
+                    <p className="mr-15 text-[100px]">“67”</p><p className="mr-15"> is more than just a number—it’s a cultural moment born from the viral song Doot Doot (6 7) by Skrilla and fueled by TikTok edits, basketball highlights, and the chaotic energy of Gen Z and Gen Alpha humor. What started as a simple chant referencing LaMelo Ball’s height and a viral clip of a kid shouting “six-seven” has exploded into classrooms, social media feeds, and everyday slang. “67” thrives on randomness, community, and the shared joy of something that makes no sense but somehow connects everyone who says it.</p>
+                </div>
             </div>
         </div>
     );
