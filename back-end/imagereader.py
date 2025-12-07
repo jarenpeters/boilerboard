c9f81d4673bd09afb7da7f97f03cc634e27bcf37
Jaren <jarenpeters10@gmail.com>
2025-12-06 19:30:47 -0500

    back end tesseract to OCR change

--- back-end/imagereader.py
+++ back-end/imagereader.py
@@ -2,21 +2,32 @@
 def readimage(matrix):
     print("importing for imagereader")
     from PIL import Image
-    import pytesseract
+    import easyocr
+    import numpy as np
     import requests
     from io import BytesIO
     from scraper import scrapeinsta
+
     print("imported for imagereader")
+
+    # Initialize EasyOCR ONCE
+    reader = easyocr.Reader(["en"], gpu=False)
+
     for i, row in enumerate(matrix):
         for j, element in enumerate(row):
             if j == 1:
                 response = requests.get(matrix[i][j])
                 image = Image.open(BytesIO(response.content))
+
                 print("converting image to b&w")
                 image = image.convert("L")
+
                 print("reading an image")
-                text = pytesseract.image_to_string(image)
+                result = reader.readtext(np.array(image), detail=0)
+                text = " ".join(result)
+
                 print("image text: " + text)
                 matrix[i].append(text)
+
     return matrix
 # ---------------- IMAGEREADER ----------------
